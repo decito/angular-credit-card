e0ac13e58133979488bb4c258808cbf6ba640532
Decio Neto <deciom.neto10@gmail.com>
2023-03-02 17:02:12 -0300

    feat: add props to alerts

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-success-alert',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./success-alert.component.css']
 })
 export class SuccessAlertComponent {
-
+  @Input() message: string;
 }
