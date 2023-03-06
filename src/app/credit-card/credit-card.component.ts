21faaffbd0e34ca523d76ea27d6087e5517bb9ac
Decio Neto <deciom.neto10@gmail.com>
2023-03-06 12:13:49 -0300

    feat: add initial card fields validation and style

--- src/app/credit-card/credit-card.component.ts
+++ src/app/credit-card/credit-card.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./credit-card.component.css']
 })
 export class CreditCardComponent {
-  cardNumber: string = "#### #### #### ####"
+  // TODO: add day/month validation, add mask to card number
+
+  cardNumber = "";
+  ownerName = "";
+  expirationMonth: number;
+  expirationYear: number;
+
+  allowDigitOnly(event: Event) {
+    console.log(event)
+    if (/\D/g.test((<HTMLInputElement>event.target).value))
+      (<HTMLInputElement>event.target).value = (<HTMLInputElement>event.target).value.replace(/\D/g, '');
+  }
+
+  allowWordOnly(event: Event) {
+    if (/([0-9])/g.test((<HTMLInputElement>event.target).value))
+      (<HTMLInputElement>event.target).value = (<HTMLInputElement>event.target).value.replace(/([0-9])/g, '')
+  }
 }
